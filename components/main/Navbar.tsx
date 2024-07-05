bb58a9678c1a7ab64a6f0484785a0f1fc7fcf870
Alonso <alonso24005@hotmail.com>
2024-07-05 09:39:51 -0500

    modificando el botón hamburguesa

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -14,7 +14,7 @@ const Navbar = () => {
   const menuRef = useRef<HTMLDivElement>(null);
 
   const toggleMenu = () => {
-    setIsMenuOpen(prevState => !prevState);
+    setIsMenuOpen(!isMenuOpen);
   };
 
   const closeMenu = () => {
@@ -116,10 +116,10 @@ const Navbar = () => {
             </nav>
           </div>
 
-          <button className="relative flex flex-col w-10 h-10 bg-transparent justify-center items-center gap-2 md:hidden" onClick={toggleMenu}>
-            <span className={`absolute bg-blue-700 h-0.5 w-full rounded transition-all duration-500 ${isMenuOpen ? 'transform rotate-45 top-1/2' : 'top-2'}`}></span>
-            <span className={`absolute bg-blue-700 h-0.5 w-full rounded transition-all duration-500 ${isMenuOpen ? 'opacity-0' : 'top-1/2'}`}></span>
-            <span className={`absolute bg-blue-700 h-0.5 w-full rounded transition-all duration-500 ${isMenuOpen ? 'transform -rotate-45 top-1/2' : 'top-8'}`}></span>
+          <button className="relative flex flex-col w-10 h-10 bg-transparent justify-center items-center gap-2 md:hidden" onClick={() => { toggleMenu(); closeMenu(); }}>
+            <span className={`absolute bg-blue-700 dark:bg-blue-600 h-0.5 w-full rounded transition-all duration-500 ${isMenuOpen ? 'transform rotate-45 top-1/2' : 'top-2'}`}></span>
+            <span className={`absolute bg-blue-700 dark:bg-blue-600 h-0.5 w-full rounded transition-all duration-500 ${isMenuOpen ? 'opacity-0' : 'top-1/2'}`}></span>
+            <span className={`absolute bg-blue-700 dark:bg-blue-600 h-0.5 w-full rounded transition-all duration-500 ${isMenuOpen ? 'transform -rotate-45 top-1/2' : 'top-8'}`}></span>
           </button>
 
           <div className="hidden md:flex items-center space-x-5">
