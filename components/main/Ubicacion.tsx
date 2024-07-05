9582e3372b816b94a876c7c65ac14f2b071d09a7
Alonso <alonso24005@hotmail.com>
2024-07-05 12:18:28 -0500

    modificando la posición del iframe

--- components/main/Ubicacion.tsx
+++ components/main/Ubicacion.tsx
@@ -10,7 +10,7 @@ const Ubicacion = () => {
         <div className="w-full md:w-1/3 text-center items-center font-semibold">
           Av. Los Próceres 125, El Rimac Lima - Perú  
         </div>
-        <div className="w-full md:w-2/3 p-3">
+        <div className="w-full md:w-2/3 pt-3">
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d738.861432097124!2d-77.036490217672!3d-12.037783381154519!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105cf344dd49021%3A0xd5bdcea0be2cf38f!2sAv.%20de%20los%20Pr%C3%B3ceres%20125%2C%20R%C3%ADmac%2015094!5e0!3m2!1ses-419!2spe!4v1719433887328!5m2!1ses-419!2spe" 
         width="100%" height="300" style={{border:0}} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
         </div>
