f55dfe533e93e77f6545a0a56da28e4f668ad3be
Alonso <alonso24005@hotmail.com>
2024-07-10 12:08:29 -0500

    modificando propiedades del market

--- components/main/Market.tsx
+++ components/main/Market.tsx
@@ -8,14 +8,14 @@ const Suscribete = () => {
   const linkMarket = 'https://transquim.com';
 
   return (
-    <div className="flex flex-col items-center text-center px-5 py-5" id="market">
-      <h1 className="text-[30px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-700">
+    <div className="flex flex-col items-center text-center px-5 pt-10 pb-5" id="market">
+      <h1 className="text-[30px] mb-3 font-semibold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-700">
         Visita nuestra tienda online
       </h1>
-      <div className="mt-4 max-w-[1240px]">
+      <div className="max-w-[1500px]">
         <MarketCard src="/img/market.webp" alt="TRANSQUIM Market" LinkMarket={linkMarket}/>
       </div>
-      <div className="mt-4 w-60 max-w-[1240px]">
+      <div className="mt-6 w-60">
         <MarketButton onClick={() => window.open(linkMarket, '_blank')}>
           Visitar Tienda
         </MarketButton>
