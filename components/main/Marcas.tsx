453104f529d6d7989ad4e25ee35b50bc8bd0a277
Alonso <alonso24005@hotmail.com>
2024-07-10 12:14:58 -0500

    añadiendo scroll infinito a las marcas

--- components/main/Marcas.tsx
+++ components/main/Marcas.tsx
@@ -1,41 +1,36 @@
-import React from "react";
-import Image from 'next/image';
+import { IDevToolsElement } from '@/components/sub/ToolMarcas'
+import { DevTools } from '@/components/sub/ToolMarcas'
 
 const Marcas = () => {
-  return (
-    <div className="flex flex-col items-center py-5">
-      <h1 className="text-[30px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-700">
-        Nuestras Marcas
-      </h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-6 lg:gap-8 mt-2 px-5 max-w-[1240px]">
-          <a href="https://transquim.com/collections/alessi" target="_blank" rel="noopener noreferrer" className="w-full h-auto">
-            <Image src="/img/alessi.webp" alt="alessi" width={550} height={400} className="transition-transform duration-300 transform hover:-translate-y-2"/>
-            <p className="dark:text-blue-400 text-blue-800 ">ALESSI</p>
-          </a>
-          <a href="https://transquim.com/collections/slender" target="_blank" rel="noopener noreferrer" className="w-full h-auto">
-            <Image src="/img/slender.webp" alt="slender" width={550} height={400} className="transition-transform duration-300 transform hover:-translate-y-2"/>
-            <p className="dark:text-blue-400 text-blue-800">SLENDER</p>
-          </a>
-          <a href="https://transquim.com/collections/chocolateria-yara" target="_blank" rel="noopener noreferrer" className="w-full h-auto">
-            <Image src="/img/yara.webp" alt="yara" width={550} height={400} className="transition-transform duration-300 transform hover:-translate-y-2"/>
-            <p className="dark:text-blue-400 text-blue-800">YARA</p>
-          </a>
-          <a href="https://transquim.com/collections/insumos-agronomos" target="_blank" rel="noopener noreferrer" className="w-full h-auto">
-            <Image src="/img/bioflux.webp" alt="bioflux" width={550} height={400} className="transition-transform duration-300 transform hover:-translate-y-2"/>
-            <p className="dark:text-blue-400 text-blue-800">BIOFLUX</p>
-          </a>
-          <a href="https://transquim.com/collections/slender-gel-reforzado" target="_blank" rel="noopener noreferrer" className="w-full h-auto">
-            <Image src="/img/slender2.webp" alt="slender2" width={550} height={400} className="transition-transform duration-300 transform hover:-translate-y-2"/>
-            <p className="dark:text-blue-400 text-blue-800">SLENDER GEL REFORZADO</p>
-          </a>
-          <a href="https://transquim.com/collections/ecovibe" target="_blank" rel="noopener noreferrer" className="w-full h-auto">
-            <Image src="/img/ecovibe.webp" alt="ecovibe" width={550} height={400} className="transition-transform duration-300 transform hover:-translate-y-2"/>
-            <p className="dark:text-blue-400 text-blue-800">ECOVIBE</p>
-          </a>
-        </div>
-    </div>
-  );
-};
+	const duplicatedTools = [...DevTools, ...DevTools]; 
+	return (
+		<div className='px-5 pt-10 pb-5'>
+			<h1 className="text-[30px] text-center mb-3 font-semibold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-700">
+        		Nuestras Marcas
+			</h1>
+			<div className='max-w-[1450px] mx-auto'>
+				<div className='w-full inline-flex flex-nowrap overflow-hidden'>
+					<ul
+						className='flex items-center justify-center [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll'>
+						{duplicatedTools.map((tool: IDevToolsElement) => {
+							return (
+								<li key={tool.id} className='mx-8'>
+									<a href={tool.href}>
+										<img
+											className={`object-fill ${tool?.className}`}
+											src={tool.src}
+											width={tool.width}
+											height={tool.height}
+										/>
+									</a>
+								</li>
+							)
+						})}
+					</ul>
+				</div>
+			</div>
+		</div>
+	)
+}
 
 export default Marcas;
-
