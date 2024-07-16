ea91384ba9c24539b71f3fc76d23f5741e70a95c
Alonso <alonso24005@hotmail.com>
2024-07-16 08:55:17 -0500

    modificando el botón de market

--- components/main/Market.tsx
+++ components/main/Market.tsx
@@ -10,14 +10,14 @@ const Suscribete = () => {
   return (
     <div className="flex flex-col items-center text-center px-5 pt-10 pb-5" id="market">
       <h1 className="text-[30px] mb-3 font-semibold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-700">
-        Visita nuestra tienda online
+        Visita nuestra tienda virtual
       </h1>
       <div className="max-w-[1500px]">
         <MarketCard src="/img/market.webp" alt="TRANSQUIM Market" LinkMarket={linkMarket}/>
       </div>
-      <div className="mt-6 w-60">
+      <div className="mt-6 w-60 text-sm">
         <MarketButton onClick={() => window.open(linkMarket, '_blank')}>
-          Visitar Tienda
+        ¡IR A TIENDA VIRTUAL!
         </MarketButton>
       </div>
     </div>
