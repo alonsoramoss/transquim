38bf211d307664cea695d351f6b7448f20c8ca70
Alonso <alonso24005@hotmail.com>
2024-07-22 08:29:19 -0500

    agregando manejador para evitar la carga del enlace

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -1,7 +1,20 @@
+"use client"
+
 import React from "react";
 
 const Footer = () => {
     const currentYear = new Date().getFullYear();
+
+    const handleLinkClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>, targetId: string) => {
+        e.preventDefault();
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+          const yOffset = -80;
+          const y = targetElement.getBoundingClientRect().top + window.pageYOffset + yOffset;
+          window.scrollTo({ top: y, behavior: 'smooth' });
+        }
+      };
+
     return (
         <div className="w-full bg-[#ff6600] text-gray-200 py-12 border-t-4 border-orange-900">
             <div className="max-w-[1500px] mx-auto px-4 sm:px-6 lg:px-8">
@@ -11,7 +24,7 @@ const Footer = () => {
                             &copy; {currentYear} Transformaciones Químicas del Perú
                         </div>
                         <div>
-                            <a href="#inicio" className="underline">
+                            <a href="#inicio" className="underline" onClick={(e) => handleLinkClick(e, 'inicio')}>
                                 Ir a Inicio
                             </a>
                         </div>
