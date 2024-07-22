dc24775c5d40b84e63de683ad4b88955d536f6be
Alonso <alonso24005@hotmail.com>
2024-07-22 08:18:32 -0500

    modificando el enlace del logo

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -71,7 +71,7 @@ const Navbar = () => {
       >
         <div className="w-full h-full flex items-center justify-between mx-auto">
           <div className="hidden md:flex items-center md:space-x-2">
-            <a href="/" onClick={(e) => handleLinkClick(e, 'inicio')}>
+            <a href="/">
               <Image src="/img/transquim.webp" alt="logo" width={60} height={60} className={theme === 'dark' ? 'icon-dark' : 'icon-light'}/>
             </a>
           </div>
@@ -79,7 +79,7 @@ const Navbar = () => {
           <div className="flex md:hidden items-center justify-between w-full">
             <ThemeToggle setTheme={setTheme}/>
             <div className="flex items-center justify-center flex-grow">
-              <a href="/" onClick={(e) => handleLinkClick(e, 'inicio')}>
+              <a href="/">
                 <Image src="/img/transquim.webp" alt="logo" width={60} height={60} className={theme === 'dark' ? 'icon-dark' : 'icon-light'}/>
               </a>
             </div>
