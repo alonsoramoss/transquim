770553a66ef37e1b01850830eabec5ef2fe51561
Alonso <alonso24005@hotmail.com>
2024-08-27 09:34:02 -0500

    modificando el color de texto

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -16,15 +16,15 @@ const Footer = () => {
       };
 
     return (
-        <div className="w-full bg-[#ff6600] text-gray-200 py-12 border-t-4 border-orange-900">
+        <div className="w-full bg-[#ff6600] text-black dark:text-white font-semibold dark:font-normal py-12 border-t-4 border-orange-900">
             <div className="max-w-[1500px] mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="text-sm md:text-base">
                     <div className="flex items-center justify-between">
                         <div className="max-w-52 md:max-w-full">
-                            &copy; {currentYear} Transformaciones Químicas del Perú
+                            <p>&copy; {currentYear} <a href="https://transquim.netlify.app" className="hover:underline"> Transformaciones Químicas del Perú </a> </p>
                         </div>
                         <div>
-                            <a href="#inicio" className="underline" onClick={(e) => handleLinkClick(e, 'inicio')}>
+                            <a href="#inicio" className="hover:underline" onClick={(e) => handleLinkClick(e, 'inicio')}>
                                 Ir a Inicio
                             </a>
                         </div>
