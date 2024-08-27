b1eb6ad8eaccc26cdac0cd0ce6e3fd69b360fac8
Alonso <alonso24005@hotmail.com>
2024-08-27 12:21:21 -0500

    quitando el borde a los cuadros en Inicio

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -12,20 +12,20 @@ const HeroContent = () => {
           <p className="text-xl text-center">
           ¿Por qué elegir <strong className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-700">TRANSQUIM</strong>?
           </p>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 pt-7">
-          <div className="flex flex-col text-center p-4 border border-blue-700 rounded-lg shadow-2xl">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 pt-7 items-center">
+          <div className="flex flex-col text-center p-4 rounded-lg shadow-2xl">
             <strong className="text-lg mb-2 text-orange-700 dark:text-orange-500">Calidad Garantizada</strong>
             <p> Fabricamos productos bajo estrictas buenas prácticas de manufactura, garantizando su eficacia y seguridad.</p>
           </div>
-          <div className="flex flex-col text-center p-4 border border-blue-700 rounded-lg shadow-2xl">
+          <div className="flex flex-col text-center p-4 rounded-lg shadow-2xl">
             <strong className="text-lg mb-2 text-orange-700 dark:text-orange-500">Experiencia y Confianza</strong>
             <p> Con más de 35 años de experiencia en la industria, hemos demostrado ser un líder confiable en la oferta de soluciones químicas de alta calidad.</p>
           </div>
-          <div className="flex flex-col text-center p-4 border border-blue-700 rounded-lg shadow-2xl">
+          <div className="flex flex-col text-center p-4 rounded-lg shadow-2xl">
             <strong className="text-lg mb-2 text-orange-700 dark:text-orange-500">Alcance Global</strong>
             <p> Estamos en constante expansión, fortaleciendo nuestras conexiones locales y ampliando nuestra red de distribución internacional para ofrecerte lo mejor donde estés.</p>
           </div>
-          <div className="flex flex-col text-center p-4 border border-blue-700 rounded-lg shadow-2xl">
+          <div className="flex flex-col text-center p-4 rounded-lg shadow-2xl">
             <strong className="text-lg mb-2 text-orange-700 dark:text-orange-500">Compromiso con la Sostenibilidad</strong>
             <p> Trabajamos de manera ética y sostenible, comprometidos con prácticas de producción responsables con el medio ambiente, minimizando el impacto ecológico y promoviendo la sostenibilidad en todas nuestras operaciones.</p>
           </div>
