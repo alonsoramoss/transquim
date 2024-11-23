fd39b1cbc9e83f9317ad0c69195c739ca4b7865e
Alonso Ramos <alonso24005@hotmail.com>
2024-11-23 18:32:55 -0500

    feat(layout): Configura ThemeProvider con tema por defecto light

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({children}: { children: React.ReactNode }) {
   return (
     <html lang="es" suppressHydrationWarning>
       <body className={`${montserrat.className} relative text-black dark:text-white overflow-y-scroll overflow-x-hidden min-h-screen flex flex-col`}>
-        <ThemeProvider attribute="class" enableSystem={true}>
+        <ThemeProvider attribute="class" defaultTheme="light">
         <div className="absolute top-0 bottom-0 z-[-2] min-h-screen w-full bg-neutral-50 dark:bg-neutral-950
           dark:bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
           <DecorativeImages/>
