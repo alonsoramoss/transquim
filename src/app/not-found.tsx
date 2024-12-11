efa926d4c766a877b973d7951fd24a6bd01e1c27
Alonso Ramos <alonso24005@hotmail.com>
2024-12-11 00:44:56 -0500

    feat: agrega página de error 404

---
+++ src/app/not-found.tsx
@@ -0,0 +1,29 @@
+import Navbar404 from "@/components/main/Navbar404";
+
+export const metadata = {
+  title: "Página no encontrada - Transformaciones Químicas del Perú",
+  description:
+    "Soluciones innovadoras en los sectores de salud, alimentación, cuidado personal, limpieza y agricultura.",
+};
+
+export default function NotFound() {
+  return (
+    <>
+      <Navbar404/>
+      <div className="absolute inset-0 flex items-center justify-center mb-20">
+        <div className="text-center p-8 md:p-16 w-full max-w-2xl">
+          <h1 className="text-5xl md:text-6xl font-bold mb-5"> Error 404 </h1>
+          <p className="text-lg md:text-2xl font-medium uppercase"> Página no encontrada </p>
+          <p className="text-base md:text-lg mt-5">
+            ¡Hola! No hemos encontrado lo que buscabas. Verifica que la URL sea correcta o usa el botón de abajo para volver a nuestro sitio web.
+          </p>
+          <div className="mt-8">
+            <a href="/" className="px-6 py-3 bg-blue-600 text-white rounded-full font-semibold hover:bg-blue-800 transition duration-300">
+              Volver a inicio
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/components/main/Navbar404.tsx
@@ -0,0 +1,174 @@
+"use client";
+
+import { useState, useEffect, useRef } from "react"
+import { useTheme } from "next-themes"
+import Image from "next/image"
+import ThemeToggle from "./ThemeToggle"
+import { Sora } from "next/font/google"
+import { Socials } from "@/constants/index"
+import { AnimatePresence, motion } from "framer-motion"
+import { slideInFromTop } from "@/utils/motion"
+
+const sora = Sora({ subsets: ["latin"] });
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { theme } = useTheme();
+  const menuRef = useRef<HTMLDivElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
+  const handleLinkClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>, targetId: string) => {
+    e.preventDefault();
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      const yOffset = -75;
+      const y = targetElement.getBoundingClientRect().top + window.pageYOffset + yOffset;
+      window.scrollTo({ top: y, behavior: "smooth" });
+      closeMenu();
+    }
+  };
+
+  useEffect(() => {
+    const handleOutsideClick = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node) &&
+          buttonRef.current && !buttonRef.current.contains(event.target as Node)) {
+        closeMenu();
+      }
+    };
+
+    if (isMenuOpen) {
+      document.addEventListener("mousedown", handleOutsideClick);
+    } else {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [isMenuOpen]);
+
+  return (
+    <>
+      <div
+        className="w-full h-20 fixed top-0 transition-colors duration-300 z-50 px-5 bg-[#ff8000] backdrop-blur-md shadow-lg">
+        <div className="w-full h-full flex items-center justify-between mx-auto">
+          <div className="hidden md:flex items-center md:space-x-2">
+            <a href="/">
+              <Image src="/img/transquim.webp" alt="logo" width={60} height={60} className="pointer-events-none"/>
+            </a>
+          </div>
+
+          <div className="flex md:hidden items-center justify-between w-full">
+            <ThemeToggle/>
+            <div className="flex items-center justify-center flex-grow">
+              <a href="/">
+                <Image src="/img/transquim.webp" alt="logo" width={60} height={60} className="pointer-events-none"/>
+              </a>
+            </div>
+          </div>
+
+          <div className="hidden md:flex md:w-[500px] h-full items-center justify-between mx-16">
+            <nav className={`${sora.className} flex items-center justify-between w-full h-auto px-5 text-neutral-950 dark:text-neutral-50`}>
+              <a href="#inicio" className="link font-medium text-sm" onClick={(e) => handleLinkClick(e, "inicio")}>
+                INICIO
+              </a>
+              <a href="#market" className="link font-medium text-sm" onClick={(e) => handleLinkClick(e, "market")}>
+                MARKET
+              </a>
+              <a href="#ubicacion" className="link font-medium text-sm" onClick={(e) => handleLinkClick(e, "ubicacion")}>
+                UBICACIÓN
+              </a>
+              <a href="#contacto" className="link font-medium text-sm" onClick={(e) => handleLinkClick(e, "contacto")}>
+                CONTACTO
+              </a>
+            </nav>
+          </div>
+
+          <button
+            aria-label="menu" ref={buttonRef}
+            className="relative flex flex-col w-10 h-10 bg-transparent justify-center items-center gap-2 md:hidden" onClick={toggleMenu}>
+            <span className={`absolute bg-blue-700 dark:bg-blue-600 h-0.5 w-full rounded transition-all duration-500 ${isMenuOpen ? "transform rotate-45 top-1/2" : "top-2"}`}></span>
+            <span className={`absolute bg-blue-700 dark:bg-blue-600 h-0.5 w-full rounded transition-all duration-500 ${isMenuOpen ? "opacity-0" : "top-1/2"}`}></span>
+            <span className={`absolute bg-blue-700 dark:bg-blue-600 h-0.5 w-full rounded transition-all duration-500 ${isMenuOpen ? "transform -rotate-45 top-1/2" : "top-8"}`}></span>
+          </button>
+
+          <div className="hidden md:flex items-center space-x-5">
+            {Socials.map((social) => (
+              <div key={social.name} className="relative group">
+                <a href={social.url} target="_blank" key={social.name} className="relative group">
+                  {theme && (
+                    <Image 
+                      src={theme === "dark" ? `/svg/light-${social.src}` : `/svg/dark-${social.src}`}
+                      alt={social.name}
+                      key={social.name}
+                      width={social.size.width}
+                      height={social.size.height}
+                      className="pointer-events-none"/>
+                  )}
+                </a>
+                <span className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-full opacity-0 group-hover:opacity-100 text-sm bg-black text-white rounded px-2 py-1 transition-opacity duration-300">
+                  {social.name}
+                </span>
+              </div>
+            ))}
+            <ThemeToggle/>
+          </div>
+        </div>
+
+        <AnimatePresence>
+          {isMenuOpen && (
+            <motion.div
+              ref={menuRef}
+              key="mobile-menu"
+              variants={slideInFromTop}
+              initial="hidden"
+              animate="visible"
+              exit="hidden"
+              className="absolute right-0 top-20 w-40 bg-[#ff8000] text-white md:hidden">
+              <nav className="flex flex-col items-center text-neutral-950 dark:text-neutral-50">
+                <a href="#inicio" className="link font-medium text-sm pt-4 pb-2" onClick={(e) => { handleLinkClick(e, "inicio"); closeMenu(); }}>
+                  INICIO
+                </a>
+                <a href="#market" className="link font-medium text-sm py-2" onClick={(e) => { handleLinkClick(e, "market"); closeMenu(); }}>
+                  MARKET
+                </a>
+                <a href="#ubicacion" className="link font-medium text-sm py-2" onClick={(e) => { handleLinkClick(e, "ubicacion"); closeMenu(); }}>
+                  UBICACIÓN
+                </a>
+                <a href="#contacto" className="link font-medium text-sm py-2" onClick={(e) => { handleLinkClick(e, "contacto"); closeMenu(); }}>
+                  CONTACTO
+                </a>
+              </nav>
+              <div className="flex py-4 justify-center">
+                {Socials.map((social) => (
+                  <a href={social.url} target="_blank" key={social.name} className="relative group mx-3">
+                    <Image
+                      src={theme === "dark" ? `/svg/light-${social.src}` : `/svg/dark-${social.src}`}
+                      alt={social.name}
+                      key={social.name}
+                      width={social.size.width}
+                      height={social.size.height}
+                      className="pointer-events-none"/>
+                    <span className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-full opacity-0 group-hover:opacity-100 text-sm bg-black text-white rounded px-2 py-1 transition-opacity duration-300">
+                      {social.name}
+                    </span>
+                  </a>
+                ))}
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
