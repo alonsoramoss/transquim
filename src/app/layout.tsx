9f4b32d09cfdf090ab52eac7d1413c95f645eb5a
Alonso Ramos <alonso24005@hotmail.com>
2025-04-11 14:34:26 -0500

    feat: agrega ClientThemeWrapper para la gestión de temas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,38 @@
-import type { Metadata } from "next"
-import { Montserrat } from "next/font/google"
-import { ThemeProvider } from "next-themes"
-import DecorativeImages from "@/components/sub/DecorativeImages"
-import Navbar from "@/components/main/Navbar"
-import Footer from "@/components/main/Footer"
-import "@/styles/globals.css"
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import ClientThemeWrapper from "@/components/utils/ClientThemeWrapper";
+import DecorativeImages from "@/components/sub/DecorativeImages";
+import Navbar from "@/components/main/Navbar";
+import Footer from "@/components/main/Footer";
+import "@/styles/globals.css";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Transformaciones Químicas del Perú",
   description: "Soluciones innovadoras en los sectores de salud, alimentación, cuidado personal, limpieza y agricultura.",
-  icons:{
-    icon:["/favicon.ico?v=4"],
-    apple:["/apple-touch-icon.png?v=4"],
-    shortcut:["/apple-touch-icon.png"]
+  icons: {
+    icon: ["/favicon.ico?v=4"],
+    apple: ["/apple-touch-icon.png?v=4"],
+    shortcut: ["/apple-touch-icon.png"]
   }
 };
 
-export default function RootLayout({children}: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="es">
+    <html lang="es" suppressHydrationWarning>
       <body className={`${montserrat.className} relative text-black dark:text-white overflow-y-scroll overflow-x-hidden min-h-screen flex flex-col`}>
-        <ThemeProvider attribute="class" defaultTheme="light">
+        <ClientThemeWrapper>
           <div className="absolute top-0 bottom-0 z-[-2] min-h-screen w-full bg-neutral-50 dark:bg-neutral-950
             dark:bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
-            <DecorativeImages/>
+            <DecorativeImages />
           </div>
-          <Navbar/>
+          <Navbar />
           <div className="flex-grow">
             {children}
           </div>
-          <Footer/>
-        </ThemeProvider>
+          <Footer />
+        </ClientThemeWrapper>
       </body>
     </html>
   );
