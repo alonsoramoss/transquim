ddda201f7df2e418c7aedff5942a913af6f0847f
Alonso Ramos <alonso24005@hotmail.com>
2025-04-29 21:54:24 -0500

    feat: actualiza site.webmanifest y el layout para mejorar el seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import ClientThemeWrapper from "@/components/utils/ClientThemeWrapper";
 import DecorativeImages from "@/components/sub/DecorativeImages";
@@ -8,20 +7,42 @@ import "@/styles/globals.css";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Transformaciones Químicas del Perú",
-  description: "Soluciones innovadoras en los sectores de salud, alimentación, cuidado personal, limpieza y agricultura.",
-  icons: {
-    icon: ["/favicon.ico?v=4"],
-    apple: ["/apple-touch-icon.png?v=4"],
-    shortcut: ["/apple-touch-icon.png"]
-  }
-};
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: {
+  children: React.ReactNode
+}) {
   return (
-    <html lang="es" suppressHydrationWarning>
-      <body className={`${montserrat.className} relative text-black dark:text-white overflow-y-scroll overflow-x-hidden min-h-screen flex flex-col`}>
+    <html lang="es">
+      <head>
+        <title>Transformaciones Químicas del Perú</title>
+        <meta name="description" content="Soluciones innovadoras en los sectores de salud, alimentación, cuidado personal, limpieza y agricultura." />
+        <meta name="author" content="Alonso Ramos" />
+        <meta name="creator" content="Alonso Ramos" />
+
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png" />
+        <link rel="icon" href="/favicon-16x16.png" sizes="16x16" type="image/png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="shortcut icon" href="/android-chrome-192x192.png" />
+
+        <link rel="manifest" href="/site.webmanifest" />
+
+        <meta name="google-site-verification" content="tUbG3Yc-yQvaSMjgz4wvH2WVo2JWFF2DRewB0Js1DB8" />
+
+        <meta property="og:url" content="https://transquim.vercel.app" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Transformaciones Químicas del Perú" />
+        <meta property="og:description" content="Soluciones innovadoras en los sectores de salud, alimentación, cuidado personal, limpieza y agricultura." />
+        <meta property="og:image" content="https://transquim.vercel.app/og.jpg" />
+        <meta property="og:site_name" content="Transformaciones Químicas del Perú" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="transquim.vercel.app" />
+        <meta property="twitter:url" content="https://transquim.vercel.app" />
+        <meta name="twitter:title" content="Transformaciones Químicas del Perú" />
+        <meta name="twitter:description" content="Soluciones innovadoras en los sectores de salud, alimentación, cuidado personal, limpieza y agricultura." />
+        <meta name="twitter:image" content="https://transquim.vercel.app/og.jpg" />
+      </head>
+      <body suppressHydrationWarning className={`${montserrat.className} relative text-black dark:text-white overflow-y-scroll overflow-x-hidden min-h-screen flex flex-col`}>
         <ClientThemeWrapper>
           <div className="absolute top-0 bottom-0 z-[-2] min-h-screen w-full bg-neutral-50 dark:bg-neutral-950
             dark:bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
