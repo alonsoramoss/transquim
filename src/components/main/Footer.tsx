1bd0ba4b305f5c317b86c00bdf32111d44954524
Alonso Ramos <alonso24005@hotmail.com>
2025-07-27 03:48:47 -0500

    style(footer): ajusta el ancho máximo y cambia el texto del enlace

--- src/components/main/Footer.tsx
+++ src/components/main/Footer.tsx
@@ -15,7 +15,7 @@ const Footer = () => {
 
     return (
         <div className="w-full bg-[#ff8000] text-black dark:text-white py-12 border-t-4 border-orange-700">
-            <div className="max-w-[1500px] mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="max-w-7xl mx-auto px-5">
                 <div className="text-sm md:text-base">
                     <div className="flex items-center justify-between">
                         <div className="max-w-52 md:max-w-full">
@@ -23,7 +23,7 @@ const Footer = () => {
                         </div>
                         <div>
                             <a href="#inicio" className="underline" onClick={(e) => handleLinkClick(e, "inicio")}>
-                                Ir a Inicio
+                                Volver arriba
                             </a>
                         </div>
                     </div>
