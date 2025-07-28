32e76dd42c99450cfb669c4f538cae6daa4aebb8
Alonso Ramos <alonso24005@hotmail.com>
2025-07-28 02:38:02 -0500

    feat: reemplaza iframe con el componente Map para mejorar la visualización de la ubicación

--- src/components/main/Ubicacion.tsx
+++ src/components/main/Ubicacion.tsx
@@ -1,4 +1,5 @@
 import { Sora } from "next/font/google"
+import Map from "../sub/Map";
 
 const sora = Sora({ subsets: ["latin"] });
 
@@ -12,14 +13,11 @@ const Ubicacion = () => {
       <div className="flex flex-col md:flex-row items-center justify-center w-full mx-auto max-w-7xl gap-x-4 px-5">
         <div className="flex flex-col md:w-1/3 text-lg items-center font-semibold py-8 gap-4">
           Av. de los Próceres 125, El Rimac Lima - Perú
-          <a href="https://maps.google.com?daddr=Av. de los Próceres 125, Lima 15094" target="_blank" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors duration-300">
+          <a href="https://maps.google.com?daddr=Av. de los Próceres 125, Lima 15094" target="_blank" className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition-colors duration-300">
           Dirección</a>
         </div>
         <div className="w-full md:w-2/3">
-          <iframe title="Mapa de la ubicación de TRANSQUIM"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d738.861432097124!2d-77.036490217672!3d-12.037783381154519!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105cf344dd49021%3A0xd5bdcea0be2cf38f!2sAv.%20de%20los%20Pr%C3%B3ceres%20125%2C%20R%C3%ADmac%2015094!5e0!3m2!1ses-419!2spe!4v1719433887328!5m2!1ses-419!2spe" 
-          width="100%" height="500" loading="lazy" className="rounded border-0" referrerPolicy="no-referrer-when-downgrade">
-          </iframe>
+          <Map />
         </div>
       </div>
       <div className="flex space-x-4 mt-20">
