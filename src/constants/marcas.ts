df1bcd9103c573f60c943ef0d25ef6a16a6ed336
Alonso Ramos <alonso24005@hotmail.com>
2025-08-28 03:44:57 -0500

    refactor(constants): renombra ToolMarcas.tsx a marcas.ts y tipa el array de marcas

--- src/constants/marcas.ts
+++ src/constants/marcas.ts
@@ -1,63 +1,59 @@
+import type { Marca } from "@/types/marca";
 import { v4 as uuid } from "uuid"
 
-export interface IDevToolsElement {
-    id: string
-	href: string
-    alt: string
-    className?: string
-	src: string
-	width: string
-    height: string
-}
-
-export const DevTools: IDevToolsElement[] = [
+export const MARCAS: Marca[] = [
     {
         id: uuid(),
+        name: "Alessi",
         href: "https://transquim.com/collections/alessi",
-        alt: "Alessi - Alcoholes, jabones y productos de limpieza",
         src: "/img/marcas/alessi.webp",
-        width: "550",
+        alt: "Alessi - Alcoholes, jabones y productos de limpieza",
         height: "228",
+        width: "550",
     },
     {
         id: uuid(),
+        name: "Slender",
         href: "https://transquim.com/collections/slender",
-        alt: "Slender - Productos saludables para un estilo de vida sano",
         src: "/img/marcas/slender.webp",
-        width: "550",
+        alt: "Slender - Productos saludables para un estilo de vida sano",
         height: "228",
+        width: "550",
     },
     {
         id: uuid(),
+        name: "Yara",
         href: "https://transquim.com/collections/chocolateria-yara",
-        alt: "Yara - Chocolate peruano con cacao orgánico fino de aroma",
         src: "/img/marcas/yara.webp",
-        width: "550",
+        alt: "Yara - Chocolate peruano con cacao orgánico fino de aroma",
         height: "228",
+        width: "550",
     },
     {
         id: uuid(),
+        name: "Slender Gel Reforzado",
         href: "https://transquim.com/collections/slender-gel-reforzado",
-        alt: "Slender Gel Reforzado - Gel tonificante que refresca y humecta la piel",
         src: "/img/marcas/slender2.webp",
-        width: "550",
+        alt: "Slender Gel Reforzado - Gel tonificante que refresca y humecta la piel",
         height: "228",
+        width: "550",
     },
     {
         id: uuid(),
+        name: "Ecovibe",
         href: "https://transquim.com/collections/ecovibe",
-        alt: "Ecovibe - Gel de ecografía y ultrasonido, no alergénico y fácil de aplicar",
         src: "/img/marcas/ecovibe.webp",
-        width: "550",
+        alt: "Ecovibe - Gel de ecografía y ultrasonido, no alergénico y fácil de aplicar",
         height: "228",
+        width: "550",
     },
     {
         id: uuid(),
+        name: "Bioflux",
         href: "https://transquim.com/collections/insumos-agronomos",
-        alt: "Bioflux - Agroquímicos y soluciones de tratamiento de aguas residuales",
         src: "/img/marcas/bioflux.webp",
-        width: "550",
+        alt: "Bioflux - Agroquímicos y soluciones de tratamiento de aguas residuales",
         height: "228",
+        width: "550",
     },
 ];
-
