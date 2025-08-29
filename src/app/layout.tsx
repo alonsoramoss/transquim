c70eb40c2496eff26d8807f97ac12865e0f0b97a
Alonso Ramos <alonso24005@hotmail.com>
2025-08-29 03:28:04 -0500

    fix(layout): agrega suppressHydrationWarning y ajusta estilos del body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,12 +58,10 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({ children }: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="es">
-      <body className={`${montserrat.className} text-black dark:text-white flex flex-col min-h-screen`}>
+    <html lang="es" suppressHydrationWarning>
+      <body className={`${montserrat.className} flex flex-col min-h-screen`}>
         <ClientThemeWrapper>
           <div className="absolute top-0 bottom-0 z-[-2] min-h-screen w-full bg-neutral-50 dark:bg-neutral-950
             dark:bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
