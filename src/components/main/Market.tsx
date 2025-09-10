2f9fa90e9e6d11ae49a009d47b9e7210ff757732
Alonso Ramos <alonso24005@hotmail.com>
2025-09-10 01:46:21 -0500

    refactor(Market): renombra componente y ajusta ancho de línea

--- src/components/main/Market.tsx
+++ src/components/main/Market.tsx
@@ -3,7 +3,7 @@
 import MarketCard from "../sub/MarketCard"
 import MarketButton from "../sub/MarketButton"
 
-const Suscribete = () => {
+const Market = () => {
   const linkMarket = "https://transquim.com";
 
   return (
@@ -11,7 +11,7 @@ const Suscribete = () => {
       <h1 className="font-heading text-3xl md:text-5xl px-5 md:px-10 font-extrabold text-green-700 dark:text-lime-400">
         ¡VISITA NUESTRA TIENDA EN LÍNEA!
       </h1>
-      <div className="h-1 w-16 my-4 md:my-8 bg-teal-500 animate-pulse rounded"/>
+      <div className="h-1 w-28 my-4 md:my-8 bg-teal-500 animate-pulse rounded"/>
       <div className="max-w-[1440px] px-5">
         <MarketCard src="/img/market.webp" alt="TRANSQUIM Market" LinkMarket={linkMarket}/>
       </div>
@@ -24,4 +24,4 @@ const Suscribete = () => {
   );
 };
 
-export default Suscribete;
+export default Market;
