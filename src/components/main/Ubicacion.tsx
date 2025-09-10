e9031be6fb283756af92395152e1b39e6a3d75c4
Alonso Ramos <alonso24005@hotmail.com>
2025-09-10 01:50:36 -0500

    style(Ubicacion): cambia animación de línea de pulse a ping

--- src/components/main/Ubicacion.tsx
+++ src/components/main/Ubicacion.tsx
@@ -6,7 +6,7 @@ const Ubicacion = () => {
       <h1 className="font-heading text-4xl md:text-5xl pt-1 font-bold text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-700">
         UBÍCANOS
       </h1>
-      <div className="h-1 w-32 mt-4 md:my-8 bg-indigo-500 animate-pulse rounded"/>
+      <div className="h-1 w-32 mt-4 md:my-8 bg-indigo-500 animate-ping rounded"/>
       <div className="flex flex-col md:flex-row items-center justify-center w-full mx-auto max-w-[1440px] gap-x-4 px-5">
         <div className="flex flex-col md:w-1/3 text-lg items-center font-semibold py-8 gap-4">
           Av. de los Próceres 125, El Rimac Lima - Perú
