951793fc8f662a9f4e4ea83f1a7a842d855ab700
Alonso Ramos <alonso24005@hotmail.com>
2025-09-11 21:18:47 -0500

    refactor(Contacto): reorganiza estructura y ajusta estilos

--- src/components/main/Contacto.tsx
+++ src/components/main/Contacto.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 
 const Contacto = () => {
   return (
-    <section id="contacto" className="flex flex-col md:items-center mx-auto pt-20 md:pb-40 px-0 md:px-5 max-w-[1440px]">
+    <section id="contacto" className="flex flex-col md:items-center w-full max-w-[1440px] mx-auto pt-20 px-0 md:px-5 md:pb-40">
       <div className="flex flex-col bg-gradient-to-r from-[#fb7d42] to-[#fdc87d] dark:from-[#ab491b] dark:to-[#e29b36] py-10 md:py-16 px-5 md:px-10 items-center md:relative md:rounded-2xl">
         <CoolBackground/>
         <h1 className="font-heading text-4xl md:text-5xl pt-1 font-bold text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-700">
