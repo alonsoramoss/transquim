3292976f73b235b1b56d8e29429f636f4b04425a
Alonso Ramos <alonso24005@hotmail.com>
2025-09-11 19:05:18 -0500

    refactor(Footer): simplifica componente eliminando scroll y enlace de volver arriba

--- src/components/main/Footer.tsx
+++ src/components/main/Footer.tsx
@@ -1,35 +1,12 @@
-"use client"
-
 const Footer = () => {
     const currentYear = new Date().getFullYear();
 
-    const handleLinkClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>, targetId: string) => {
-        e.preventDefault();
-        const targetElement = document.getElementById(targetId);
-        if (targetElement) {
-            const yOffset = -80;
-            const y = targetElement.getBoundingClientRect().top + window.pageYOffset + yOffset;
-            window.scrollTo({ top: y, behavior: "smooth" });
-        }
-    };
-
     return (
-        <div className="w-full bg-[#ff8000] text-black dark:text-white py-12 border-t-4 border-orange-700">
-            <div className="max-w-[1440px] mx-auto px-5">
-                <div className="text-sm md:text-base">
-                    <div className="flex items-center justify-between">
-                        <div className="max-w-52 md:max-w-full">
-                            <p> &copy; {currentYear} Transformaciones Químicas del Perú </p>
-                        </div>
-                        <div>
-                            <a href="#inicio" className="underline" onClick={(e) => handleLinkClick(e, "inicio")}>
-                                Volver arriba
-                            </a>
-                        </div>
-                    </div>
-                </div>
+        <footer className="w-full bg-[#ff8000] text-black dark:text-white border-t-4 border-orange-700">
+            <div className="max-w-[1440px] mx-auto py-12 px-5 text-sm md:text-base">
+                <p> &copy; {currentYear} Transformaciones Químicas del Perú </p>
             </div>
-        </div>
+        </footer>
     )
 }
 
