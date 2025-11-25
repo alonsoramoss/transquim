620fb8ebe7486c2301f7a05fa9ac417e87b26070
Alonso Ramos <alonso24005@hotmail.com>
2025-11-25 00:13:13 -0500

    refactor(ui): reemplaza estilos de títulos por el componente MagicText

--- src/components/main/Contacto.tsx
+++ src/components/main/Contacto.tsx
@@ -1,4 +1,5 @@
 import CoolBackground from "../sub/CoolBackground"
+import MagicText from "../sub/MagicText"
 import Image from "next/image"
 
 const Contacto = () => {
@@ -6,9 +7,9 @@ const Contacto = () => {
     <section id="contacto" className="flex flex-col md:items-center w-full max-w-[1440px] mx-auto pt-20 px-0 md:px-5 md:pb-40">
       <div className="flex flex-col bg-gradient-to-r from-[#fb7d42] to-[#fdc87d] dark:from-[#ab491b] dark:to-[#e29b36] py-10 md:py-16 px-5 md:px-10 items-center md:relative md:rounded-2xl">
         <CoolBackground/>
-        <h1 className="font-heading text-4xl md:text-5xl pt-1 font-bold text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-700">
-          CONTÁCTANOS
-        </h1>
+        <h2 className="font-heading text-4xl md:text-5xl font-bold">
+          <MagicText>CONTÁCTANOS</MagicText>
+        </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-5 py-8">
           <div>
             📞 Telf: (511) 381 - 4246 <br /> 
